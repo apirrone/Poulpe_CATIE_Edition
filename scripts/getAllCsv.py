ab36105ecad55d79e286ec0bcfddcec2104e4ef3
apirrone <awanegenebayo@gmail.com>
2018-08-06 14:42:32 +0200

    getAllCsv.py

---
+++ scripts/getAllCsv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import re
+import os
+
+file="../README.md"
+with open(file) as f:
+    lines = f.readlines()
+    links = []
+    for l in lines:
+        link = re.findall(r'.*https.*', l)
+        if(link):
+            links.append(link)
+
+    outDir = 'csv/'
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+            
+    for l in links:
+        string = str(l)[2:][:-2]
+        downloadLink = string+'/download'
+        print(downloadLink)
+        os.system('wget -P '+outDir+' '+downloadLink)
+        
+        
+
+    
+
+    
+        
